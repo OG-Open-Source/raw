bbe116ec4bf72e3c092400525250138800bbcbd7
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-06 17:14:50 +0800

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-GRAY()   { echo -e "\e[37m$1\e[0m"; }
-RED()    { echo -e "\e[31m$1\e[0m"; }
-YELLOW() { echo -e "\e[33m$1\e[0m"; }
-CYAN()   { echo -e "\e[96m$1\e[0m"; }
-GREEN()  { echo -e "\e[32m$1\e[0m"; }
+Version="1.0.0.0"
+Copyright="OG|OS - OGATA-Open-Source"
+
+GRAY() {
+    echo -e "\e[37m$1\e[0m"
+}
+
+RED() {
+    echo -e "\e[31m$1\e[0m"
+}
+
+YELLOW() {
+    echo -e "\e[33m$1\e[0m"
+}
+
+CYAN() {
+    echo -e "\e[96m$1\e[0m"
+}
+
+GREEN() {
+    echo -e "\e[32m$1\e[0m"
+}
