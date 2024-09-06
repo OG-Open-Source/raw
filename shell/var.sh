28d20fb1d609bbf4733bf84fe6f75803fd35db23
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-06 16:44:08 +0800

    Create var.sh

---
+++ shell/var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GRAY()   { echo -e "\e[37m$1\e[0m"; }
+RED()    { echo -e "\e[31m$1\e[0m"; }
+YELLOW() { echo -e "\e[33m$1\e[0m"; }
+CYAN()   { echo -e "\e[96m$1\e[0m"; }
+GREEN()  { echo -e "\e[32m$1\e[0m"; }
