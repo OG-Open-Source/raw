832e6254c73da98b4e5efaa13b2ad91eaf5d2f12
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-07 01:01:06 +0800

    Update

--- shell/function.sh
+++ shell/function.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 # © 2024 OG|OS OGATA-Open-Source
+# curl -sSL https://raw.ogtt.tk/shell/function.sh -o function.sh
+# source function.sh
 
-GRAY() { echo -e "\e[37m$1\e[0m"; }
-RED() { echo -e "\e[31m$1\e[0m"; }
-YELLOW() { echo -e "\e[33m$1\e[0m"; }
-CYAN() { echo -e "\e[96m$1\e[0m"; }
-GREEN() { echo -e "\e[32m$1\e[0m"; }
+CLEAN() { cd ~; clear; }
+INPUT() { read -p "$1" "$2"; }
 
+FONT() {
+    local FONT=""
+    declare -A STYLE=(
+        [B]="\e[1m" [U]="\e[4m" [BLACK]="\e[30m"
+        [RED]="\e[31m" [GREEN]="\e[32m" [YELLOW]="\e[33m"
+        [BLUE]="\e[34m" [PURPLE]="\e[35m" [SKYBLUE]="\e[36m"
+        [GRAY]="\e[37m" [CYAN]="\e[96m"
+    )
+
+    for arg in "$@"; do
+        FONT+="${STYLE[$arg]}"
+    done
+    echo -e "${FONT}${arg}\e[0m"
+}
+
+TEXT=()
+for i in "${!TEXT[@]}"; do
+    printf "$(FONT CYAN "%3d.") %s\n" $((i + 1)) "${TEXT[i]}"
+done
