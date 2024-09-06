021dd4630933174eac533d2bbd3d593dd3b91236
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-07 01:21:05 +0800

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -9,12 +9,14 @@ INPUT() { read -p "$1" "$2"; }
 FONT() {
     local FONT=""
     declare -A STYLE=(
-        [B]="\e[1m" [U]="\e[4m" [BLACK]="\e[30m"
-        [RED]="\e[31m" [GREEN]="\e[32m" [YELLOW]="\e[33m"
-        [BLUE]="\e[34m" [PURPLE]="\e[35m" [SKYBLUE]="\e[36m"
-        [GRAY]="\e[37m" [CYAN]="\e[96m"
+        [B]="\e[1m" [U]="\e[4m" 
+        [BLACK]="\e[30m" [RED]="\e[31m" [GREEN]="\e[32m"
+        [YELLOW]="\e[33m" [BLUE]="\e[34m" [PINK]="\e[35m"
+        [SKYBLUE]="\e[36m" [GRAY]="\e[37m" [CYAN]="\e[96m" 
+        [BG.BLACK]="\e[40m" [BG.RED]="\e[41m" [BG.GREEN]="\e[42m"
+        [BG.YELLOW]="\e[43m" [BG.BLUE]="\e[44m" [BG.PINK]="\e[45m"
+        [BG.SKYBLUE]="\e[46m" [BG.GRAY]="\e[47m"
     )
-
     for arg in "$@"; do
         FONT+="${STYLE[$arg]}"
     done
