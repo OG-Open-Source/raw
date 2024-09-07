5d82bda8fcaa595ecff664f6d01116ab6b2538a9
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-08 00:43:14 +0800

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -3,25 +3,70 @@
 # source function.sh
 COPYRIGHT() { echo "© 2024 OG|OS OGATA-Open-Source"; }
 
-CLEAN() { cd ~; clear; }
-
-FONT() {
-    local FONT=""
-    declare -A STYLE=(
-        [B]="\e[1m" [U]="\e[4m" 
-        [BLACK]="\e[30m" [RED]="\e[31m" [GREEN]="\e[32m"
-        [YELLOW]="\e[33m" [BLUE]="\e[34m" [PINK]="\e[35m"
-        [SKYBLUE]="\e[36m" [GRAY]="\e[37m" [CYAN]="\e[96m" 
-        [BG.BLACK]="\e[40m" [BG.RED]="\e[41m" [BG.GREEN]="\e[42m"
-        [BG.YELLOW]="\e[43m" [BG.BLUE]="\e[44m" [BG.PINK]="\e[45m"
-        [BG.SKYBLUE]="\e[46m" [BG.GRAY]="\e[47m"
-    )
-    for arg in "$@"; do
-        FONT+="${STYLE[$arg]}"
-    done
-    echo -e "${FONT}${arg}\e[0m"
+ADD() {
+  [ $# -eq 0 ] && return
+  for app in "$@"; do
+    echo -e "\e[33mINSTALL [$app]\e[0m"
+    case $(command -v apk || command -v apt || command -v dnf || command -v opkg || command -v pacman || command -v yum || command -v zypper) in
+      *apk) apk info "$app" &>/dev/null || { apk update && apk add "$app"; } ;;
+      *apt) dpkg -l | grep -qw "$app" || { apt update -y && apt install -y "$app"; } ;;
+      *dnf) dnf list installed "$app" &>/dev/null || { dnf -y update && dnf install -y epel-release "$app"; } ;;
+      *opkg) opkg list-installed | grep -qw "$app" || { opkg update && opkg install "$app"; } ;;
+      *pacman) pacman -Q "$app" &>/dev/null || { pacman -Syu --noconfirm && pacman -S --noconfirm "$app"; } ;;
+      *yum) yum list installed "$app" &>/dev/null || { yum -y update && yum install -y epel-release "$app"; } ;;
+      *zypper) zypper se --installed-only "$app" &>/dev/null || { zypper refresh && zypper install -y "$app"; } ;;
+      *) return ;;
+    esac
+    echo -e "\e[32mFINISHED\e[0m"
+    echo
+  done
 }
 
-INPUT() { read -p "$1" "$2"; }
+CLEAN() {
+  cd ~
+  clear
+}
 
-TIMEZONE() { readlink /etc/localtime | sed 's/^.*zoneinfo\///' 2>/dev/null; }
+DEL() {
+  [ $# -eq 0 ] && return
+  for app in "$@"; do
+    echo -e "\e[33mREMOVE [$app]\e[0m"
+    case $(command -v apk || command -v apt || command -v dnf || command -v opkg || command -v pacman || command -v yum || command -v zypper) in
+      *apk) apk info "$app" &>/dev/null && apk del "$app" ;;
+      *apt) dpkg -l | grep -q "^ii  $app" && apt purge -y "$app" ;;
+      *dnf) dnf list installed "$app" &>/dev/null && dnf remove -y "$app" ;;
+      *opkg) opkg list-installed | grep -q "$app" && opkg remove "$app" ;;
+      *pacman) pacman -Q "$app" &>/dev/null && pacman -Rns --noconfirm "$app" ;;
+      *yum) yum list installed "$app" &>/dev/null && yum remove -y "$app" ;;
+      *zypper) zypper se --installed-only "$app" | grep -q "$app" && zypper remove -y "$app" ;;
+      *) return ;;
+    esac
+    echo -e "\e[32mFINISHED\e[0m"
+    echo
+  done
+}
+
+FONT() {
+  local FONT=""
+  declare -A STYLE=(
+    [B]="\e[1m" [U]="\e[4m"
+    [BLACK]="\e[30m" [RED]="\e[31m" [GREEN]="\e[32m"
+    [YELLOW]="\e[33m" [BLUE]="\e[34m" [PINK]="\e[35m"
+    [SKYBLUE]="\e[36m" [GRAY]="\e[37m" [CYAN]="\e[96m"
+    [BG.BLACK]="\e[40m" [BG.RED]="\e[41m" [BG.GREEN]="\e[42m"
+    [BG.YELLOW]="\e[43m" [BG.BLUE]="\e[44m" [BG.PINK]="\e[45m"
+    [BG.SKYBLUE]="\e[46m" [BG.GRAY]="\e[47m"
+  )
+  for arg in "$@"; do
+    FONT+="${STYLE[$arg]}"
+  done
+  echo -e "${FONT}${arg}\e[0m"
+}
+
+INPUT() {
+  read -p "$1" "$2"
+}
+
+TIMEZONE() {
+  readlink /etc/localtime | sed 's/^.*zoneinfo\///' 2>/dev/null
+}
