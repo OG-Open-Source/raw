f31b9ad3990df91a1a557042103e108b782878ff
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-09-09 19:36:06 +0800

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -75,6 +75,38 @@ LINE() {
   printf '%*s' "$1" '' | tr ' ' '-'
 }
 
+SYS_CLEAN() {
+  echo -e "\e[33mPerforming system cleanup...\e[0m"
+  echo
+  case $(command -v apk || command -v apt || command -v dnf || command -v opkg || command -v pacman || command -v yum || command -v zypper) in
+    *apk) apk cache clean; rm -rf /tmp/* /var/cache/apk/* /var/log/*;;
+    *apt)
+      while fuser /var/lib/dpkg/lock-frontend &>/dev/null; do
+        echo "Waiting for dpkg lock to be released..."
+        sleep 1
+      done
+      DEBIAN_FRONTEND=noninteractive dpkg --configure -a
+      apt autoremove --purge -y
+      apt clean -y
+      apt autoclean -y
+      ;;
+    *dnf) dnf autoremove -y; dnf clean all; dnf makecache;;
+    *opkg) rm -rf /tmp/* /var/log/*; opkg update; opkg clean;;
+    *pacman) pacman -Sc --noconfirm; pacman -Scc --noconfirm;;
+    *yum) yum autoremove -y; yum clean all; yum makecache;;
+    *zypper) zypper clean --all; zypper refresh;;
+    *) return 1;;
+  esac
+  
+  if command -v journalctl &>/dev/null; then
+    journalctl --rotate
+    journalctl --vacuum-time=1d
+    journalctl --vacuum-size=500M
+  fi
+
+  find /var/log -type f -delete
+  rm -rf /tmp/*
+}
 SYS_INFO() {
   echo -e "\e[33mSystem Information:\e[0m"
   echo -e "\e[96m$(printf '%*s' "24" '' | tr ' ' '-')\e[0m"
@@ -96,6 +128,80 @@ SYS_INFO() {
   echo "Timezone: $(readlink /etc/localtime | sed 's/^.*zoneinfo\///' 2>/dev/null)"
   echo -e "\e[96m$(printf '%*s' "24" '' | tr ' ' '-')\e[0m"
   echo "Uptime: $(uptime -p | sed 's/up //')"
+}
+SYS_UPDATE() {
+  echo -e "\e[33mUpdating system software...\e[0m"
+  echo
+  case $(command -v apk || command -v apt || command -v dnf || command -v opkg || command -v pacman || command -v yum || command -v zypper) in
+    *apk) apk update && apk upgrade;;
+    *apt)
+      while fuser /var/lib/dpkg/lock-frontend &>/dev/null; do
+        echo "Waiting for dpkg lock to be released..."
+        sleep 1
+      done
+      DEBIAN_FRONTEND=noninteractive apt update -y
+      DEBIAN_FRONTEND=noninteractive apt full-upgrade -y
+      ;;
+    *dnf) dnf -y update;;
+    *opkg) opkg update && opkg upgrade;;
+    *pacman) pacman -Syu --noconfirm;;
+    *yum) yum -y update;;
+    *zypper) zypper refresh && zypper update;;
+    *) return 1;;
+  esac
+}
+SYS_UPGRADE() {
+  echo -e "\e[33mPerforming system version upgrade...\e[0m"
+  echo
+  
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    case $ID in
+      debian)
+        if [ "$VERSION_ID" = "11" ]; then
+          sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
+          apt update && apt full-upgrade -y
+          apt autoremove -y
+          echo -e "\e[32mDebian 11 has been upgraded to Debian 12\e[0m"
+        else
+          echo -e "\e[31mUpgrade for this Debian version is not currently supported\e[0m"
+        fi
+        ;;
+      ubuntu)
+        if [ "$VERSION_ID" = "22.04" ]; then
+          do-release-upgrade -d
+          echo -e "\e[32mUbuntu 22.04 has been upgraded to Ubuntu 24.04\e[0m"
+        else
+          echo -e "\e[31mUpgrade for this Ubuntu version is not currently supported\e[0m"
+        fi
+        ;;
+      centos)
+        if [ "$VERSION_ID" = "7" ]; then
+          yum install -y centos-release-scl
+          yum install -y centos-release-scl-rh
+          yum install -y rh-python36 rh-python36-python-devel
+          yum install -y epel-release yum-utils
+          yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+          yum-config-manager --enable remi-php74
+          yum update -y
+          echo -e "\e[32mCentOS 7 has been updated to the latest version\e[0m"
+        elif [ "$VERSION_ID" = "8" ]; then
+          dnf upgrade -y
+          dnf install -y epel-release
+          dnf config-manager --set-enabled PowerTools
+          dnf update -y
+          echo -e "\e[32mCentOS 8 has been updated to the latest version\e[0m"
+        else
+          echo -e "\e[31mUpgrade for this CentOS version is not currently supported\e[0m"
+        fi
+        ;;
+      *)
+        echo -e "\e[31mVersion upgrade is not supported for the current operating system\e[0m"
+        ;;
+    esac
+  else
+    echo -e "\e[31mUnable to determine the operating system version\e[0m"
+  fi
 }
 
 TIMEZONE() {
