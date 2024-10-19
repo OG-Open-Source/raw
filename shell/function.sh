4d173df1f1f7e4db9b91b04ed89f2d83f69f5b0e
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-10-19 21:27:54 +0800

    Version="4.035.001"

--- shell/function.sh
+++ shell/function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Author="OGATA Open-Source"
-Version="0.035.004"
+Version="4.035.001"
 License="MIT License"
 
 SH="function.sh"
@@ -897,5 +897,5 @@ TIMEZONE() {
 			;;
 	esac
 }
-[ -f ~/function.sh ] && source ~/function.sh || bash <(curl -sL raw.ogtt.tk/shell/update-function.sh)
+[ ! -f ~/function.sh ] && bash <(curl -sL raw.ogtt.tk/shell/update-function.sh)
 crontab -l 2>/dev/null | grep -q "0 0 \* \* \* curl -sL raw.ogtt.tk/shell/update-function.sh | bash" || (crontab -l > function-update 2>/dev/null; echo "0 0 * * * curl -sL raw.ogtt.tk/shell/update-function.sh | bash" >> function-update && crontab function-update && rm -f function-update)
