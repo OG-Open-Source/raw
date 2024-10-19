6bb328631e900f7164e58043768778930fe38e25
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-10-19 20:36:16 +0800

    Update update-function.sh

--- shell/update-function.sh
+++ shell/update-function.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ -f ~/function.sh ]; then
-	source ~/function.sh
-	exit 0
-else
-	GET() {
-		[ $# -eq 0 ] && return 1
-		url="$1"
-		[[ "$url" =~ ^(http|https|ftp):// ]] || url="https://$url"
-		output_file="${url##*/}"
-		[ -z "$output_file" ] && output_file="index.html"
-		target_dir="${2:-.}"
-		mkdir -p "$target_dir" || return 1
-		output_file="$target_dir/$output_file"
-		url=$(echo "$url" | sed -E 's#([^:])/+#\1/#g; s#^(https?|ftp):/+#\1://#')
-		curl -L -k -m 5 "$url" -o "$output_file" &>/dev/null || wget --no-check-certificate -T 5 -t 2 "$url" -O "$output_file" &>/dev/null || return 1
-	}
+GET() {
+	[ $# -eq 0 ] && return 1
+	url="$1"
+	[[ "$url" =~ ^(http|https|ftp):// ]] || url="https://$url"
+	output_file="${url##*/}"
+	[ -z "$output_file" ] && output_file="index.html"
+	target_dir="${2:-.}"
+	mkdir -p "$target_dir" || return 1
+	output_file="$target_dir/$output_file"
+	url=$(echo "$url" | sed -E 's#([^:])/+#\1/#g; s#^(https?|ftp):/+#\1://#')
+	curl -L -k -m 5 "$url" -o "$output_file" &>/dev/null || wget --no-check-certificate -T 5 -t 2 "$url" -O "$output_file" &>/dev/null || return 1
+}
 
+if [ -f ~/function.sh ]; then
+	GET https://raw.ogtt.tk/shell/function.sh &>/dev/null
+	source function.sh
+else
 	version=$(curl -sL raw.ogtt.tk/shell/function.sh | grep -oP 'Version="\K[^"]+')
 	if [[ $version == 0.* ]]; then
 		echo -e "\e[33mWarning: The current version ($version) is a development version. It is not recommended for use.\e[0m"
