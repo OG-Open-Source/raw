3f98fc7b9bebe30a7641ee58c7e11de9decc0e0a
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-11-11 21:55:21 +0800

    Update apmd.cmd

--- space/apmd.cmd
+++ space/apmd.cmd
@@ -3,9 +3,9 @@ setlocal enabledelayedexpansion
 
 :: 檢查是否提供了目錄參數
 if "%~1"=="" (
-    echo 請提供目錄路徑
-    echo 用法: apmd.cmd [目錄路徑]
-    exit /b 1
+	echo 請提供目錄路徑
+	echo 用法: apmd.cmd [目錄路徑]
+	exit /b 1
 )
 
 :: 設置目標目錄
@@ -13,8 +13,8 @@ set "TARGET_DIR=%~1"
 
 :: 檢查目錄是否存在
 if not exist "!TARGET_DIR!" (
-    echo 錯誤：目錄 "!TARGET_DIR!" 不存在
-    exit /b 1
+	echo 錯誤：目錄 "!TARGET_DIR!" 不存在
+	exit /b 1
 )
 
 :: 獲取目標目錄的名稱
@@ -37,34 +37,52 @@ for %%I in ("!current_dir!") do set "CURRENT_DIR_NAME=%%~nxI"
 
 :: 先處理子目錄（排除隱藏目錄）
 for /f "tokens=*" %%D in ('dir /b /a:d "!current_dir!" 2^>nul ^| findstr /v "^\..*"') do (
-    if not "!first_item!"=="1" (
-        echo ^>
-    )
-    echo ^>  ^> [%%D/](%%D/^)^<br^>
-    
-    :: 處理子目錄中的文件
-    pushd "!current_dir!\%%D"
-    for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*"') do (
-        echo ^>  ^>  ^> [%%F](%%D/%%F^)^<br^>
-    )
-    popd
-    
-    set "first_item=0"
+	if not "!first_item!"=="1" (
+		echo ^>
+	)
+	echo ^>  ^> [%%D/](%%D/^)^<br^>
+
+	:: 處理子目錄中的文件
+	pushd "!current_dir!\%%D"
+	:: 先處理非 README.md 和非 CNAME 文件
+	for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*" ^| findstr /v /i "^README\.md$ ^CNAME$"') do (
+		echo ^>  ^>  ^> [%%F](%%D/%%F^)^<br^>
+	)
+	:: 最後處理 README.md（如果存在）
+	if exist "README.md" (
+		echo ^>  ^>  ^> [README.md](%%D/README.md^)^<br^>
+	)
+	popd
+
+	set "first_item=0"
 )
 
-:: 如果有文件，添加分隔符並處理文件
+:: 處理當前目錄的文件
 set "has_files=0"
 pushd "!current_dir!"
-for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*"') do (
-    if not "!has_files!"=="1" (
-        if not "!first_item!"=="1" echo ^>
-        set "has_files=1"
-    )
-    if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
-        echo ^>  ^> [%%F](%%F^)^<br^>
-    ) else (
-        echo ^>  ^> [%%F](!CURRENT_DIR_NAME!/%%F^)^<br^>
-    )
+:: 先處理非 README.md 和非 CNAME 文件
+for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*" ^| findstr /v /i "^README\.md$ ^CNAME$"') do (
+	if not "!has_files!"=="1" (
+		if not "!first_item!"=="1" echo ^>
+		set "has_files=1"
+	)
+	if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
+		echo ^>  ^> [%%F](%%F^)^<br^>
+	) else (
+		echo ^>  ^> [%%F](!CURRENT_DIR_NAME!/%%F^)^<br^>
+	)
+)
+
+:: 最後處理 README.md（如果存在）
+if exist "README.md" (
+	if not "!has_files!"=="1" (
+		if not "!first_item!"=="1" echo ^>
+	)
+	if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
+		echo ^>  ^> [README.md](README.md^)^<br^>
+	) else (
+		echo ^>  ^> [README.md](!CURRENT_DIR_NAME!/README.md^)^<br^>
+	)
 )
 popd
 
