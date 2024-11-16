abd0961bb72b397cd37ddc4ced7b407634a05054
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-11-16 23:16:00 +0800

    2024.11.16

--- space/apmd.cmd
+++ space/apmd.cmd
@@ -3,9 +3,9 @@ setlocal enabledelayedexpansion
 
 :: Check if directory parameter is provided
 if "%~1"=="" (
-    echo Please provide directory path
-    echo Usage: apmd.cmd [directory_path]
-    exit /b 1
+	echo Please provide directory path
+	echo Usage: apmd.cmd [directory_path]
+	exit /b 1
 )
 
 :: Set target directory
@@ -13,8 +13,8 @@ set "TARGET_DIR=%~1"
 
 :: Check if directory exists
 if not exist "!TARGET_DIR!" (
-    echo Error: Directory "!TARGET_DIR!" does not exist
-    exit /b 1
+	echo Error: Directory "!TARGET_DIR!" does not exist
+	exit /b 1
 )
 
 :: Get target directory name
@@ -37,24 +37,24 @@ for %%I in ("!current_dir!") do set "CURRENT_DIR_NAME=%%~nxI"
 
 :: Process subdirectories first (exclude hidden directories)
 for /f "tokens=*" %%D in ('dir /b /a:d "!current_dir!" 2^>nul ^| findstr /v "^\..*"') do (
-    if not "!first_item!"=="1" (
-        echo ^>
-    )
-    echo ^>  ^> [%%D/](%%D/^)^<br^>
+	if not "!first_item!"=="1" (
+		echo ^>
+	)
+	echo ^>  ^> [%%D/](%%D/^)^<br^>
 
-    :: Process files in subdirectory
-    pushd "!current_dir!\%%D"
-    :: Process non-README.md, non-CNAME, and non-_config.yml files first
-    for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*" ^| findstr /v /i "^README\.md$ ^CNAME$ ^_config\.yml$"') do (
-        echo ^>  ^>  ^> [%%F](%%D/%%F^)^<br^>
-    )
-    :: Process README.md last (if exists)
-    if exist "README.md" (
-        echo ^>  ^>  ^> [README.md](%%D/README.md^)^<br^>
-    )
-    popd
+	:: Process files in subdirectory
+	pushd "!current_dir!\%%D"
+	:: Process non-README.md, non-CNAME, and non-_config.yml files first
+	for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*" ^| findstr /v /i "^README\.md$ ^CNAME$ ^_config\.yml$"') do (
+		echo ^>  ^>  ^> [%%F](%%D/%%F^)^<br^>
+	)
+	:: Process README.md last (if exists)
+	if exist "README.md" (
+		echo ^>  ^>  ^> [README.md](%%D/README.md^)^<br^>
+	)
+	popd
 
-    set "first_item=0"
+	set "first_item=0"
 )
 
 :: Process files in current directory
@@ -62,27 +62,27 @@ set "has_files=0"
 pushd "!current_dir!"
 :: Process non-README.md, non-CNAME, and non-_config.yml files first
 for /f "tokens=*" %%F in ('dir /b /a:-d 2^>nul ^| findstr /v "^\..*" ^| findstr /v /i "^README\.md$ ^CNAME$ ^_config\.yml$"') do (
-    if not "!has_files!"=="1" (
-        if not "!first_item!"=="1" echo ^>
-        set "has_files=1"
-    )
-    if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
-        echo ^>  ^> [%%F](%%F^)^<br^>
-    ) else (
-        echo ^>  ^> [%%F](!CURRENT_DIR_NAME!/%%F^)^<br^>
-    )
+	if not "!has_files!"=="1" (
+		if not "!first_item!"=="1" echo ^>
+		set "has_files=1"
+	)
+	if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
+		echo ^>  ^> [%%F](%%F^)^<br^>
+	) else (
+		echo ^>  ^> [%%F](!CURRENT_DIR_NAME!/%%F^)^<br^>
+	)
 )
 
 :: Process README.md last (if exists)
 if exist "README.md" (
-    if not "!has_files!"=="1" (
-        if not "!first_item!"=="1" echo ^>
-    )
-    if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
-        echo ^>  ^> [README.md](README.md^)^<br^>
-    ) else (
-        echo ^>  ^> [README.md](!CURRENT_DIR_NAME!/README.md^)^<br^>
-    )
+	if not "!has_files!"=="1" (
+		if not "!first_item!"=="1" echo ^>
+	)
+	if "!CURRENT_DIR_NAME!"=="%DIR_NAME%" (
+		echo ^>  ^> [README.md](README.md^)^<br^>
+	) else (
+		echo ^>  ^> [README.md](!CURRENT_DIR_NAME!/README.md^)^<br^>
+	)
 )
 popd
 
